a3abbf41da12e64a07c818e75a63624d69ed2eaa
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-11-26 16:37:37 -0800

    add user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+//import user-controller functions
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+// GET all /api/users
+router
+    .router('/')
+    .get(getAllUser)
+    .post(createUser);
+
+// GET one, PUT, DELETE  on /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// POST and DELETE on /api/users/:userId/friends/:friendId
+router
+    .route('/:id/friends/:friendId')
+    .put(addFriend)
+    .delete(removeFriend)
+
+// export router
+module.exports = router;
