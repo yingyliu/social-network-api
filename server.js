2afc2a5d9f903209a2dec67ed47d6e30e068cef3
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-11-26 17:20:20 -0800

    added content to server file

---
+++ server.js
@@ -0,0 +1,26 @@
+// require express and mongoose
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlendcoded({ extended: true}));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// connect to mongoose
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/social-network-api',
+    {
+        userNewUrlParser:true,
+        userUnifiedTopology: true,
+    }
+);
+
+// set this to true to log mongoose queries being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected and listening on local host:${PORT}`));
